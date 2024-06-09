09471a2691a543d3e65e2a7d6ab778ca3ccfe1dd
Carl-Rabbit <479843162@qq.com>
2024-06-09 21:17:24 +0800

    finish 005: real mode print

--- src/boot.asm
+++ src/boot.asm
@@ -9,24 +9,28 @@ mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-mov ax, 0xb800
-mov ds, ax
-mov byte [0], 'H'
-mov byte [2], 'e'
-mov byte [4], 'l'
-mov byte [6], 'l'
-mov byte [8], 'o'
-mov byte [10], ' '
-mov byte [12], 'w'
-mov byte [14], 'o'
-mov byte [16], 'r'
-mov byte [18], 'l'
-mov byte [20], 'd'
-mov byte [22], '!'
-mov byte [24], '!'
+xchg bx, bx;    boxhs magic breakpiont
 
+mov si, booting
+call print
 
 jmp $
+
+print:
+    mov ah, 0x0e
+.next:
+    mov al, [si]
+    cmp al, 0
+    jz .done
+    int 0x10
+    inc si
+    jmp .next
+.done:
+    ret
+
+booting:
+    db "Booting Yonix...", 10, 13, 0; \n\r
+    
 
 times 510 - ($ - $$) db 0
 
