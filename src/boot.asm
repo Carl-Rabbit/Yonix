eb76cc391b64c810eceb8cba10617af3b44eeaf9
Carl-Rabbit <479843162@qq.com>
2024-06-15 15:58:39 +0800

    finish 006: disk io

--- src/boot.asm
+++ src/boot.asm
@@ -1,20 +1,187 @@
 [org 0x7c00]
 
+; set the screen to be text mod, and clear the screen
 mov ax, 3
 int 0x10
 
+; init register
 mov ax, 0
 mov ds, ax
 mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-xchg bx, bx;    boxhs magic breakpiont
-
 mov si, booting
 call print
 
+xchg bx, bx; boxhs magic breakpiont
+
+mov edi, 0x1000; the target memory to store the data read from disk
+mov ecx, 0; the start sector
+mov bl, 1; number of sectors
+call read_disk
+
+xchg bx, bx; boxhs magic breakpiont
+
+mov edi, 0x1000; the target memory of data to write to disk
+mov ecx, 2; the start sector
+mov bl, 1; number of sectors
+call write_disk
+
+xchg bx, bx; boxhs magic breakpiont
+
+; block
 jmp $
+
+read_disk:
+
+    ; set the number of sectors
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx; 0x1f3
+    mov al, cl; first 8 bits of start sector
+    out dx, al
+
+    inc dx; 0x1f4
+    shr ecx, 8
+    mov al, cl; second 8 bits of start sector
+    out dx, al
+
+    inc dx; 0x1f5
+    shr ecx, 8
+    mov al, cl; third 8 bits of start sector
+    out dx, al
+
+    inc dx; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111; set the high 4 bits to be 0
+
+    mov al, 0b1110_0000
+    ;         7654 4321
+    ; 4: 0, master disk
+    ; 5/7: 1, fixed 1
+    ; 6: 1, LBA mode
+    or al, cl
+    out dx, al; master disk - LBA mode
+
+    inc dx; 0x1f7
+    mov al, 0x20; read disk
+    out dx, al
+
+    xor ecx, ecx; use xor to clear ecx, = mov ecx, 0
+    mov cl, bl; get the number of sector read
+
+    .read:
+        push cx; save cx
+        call .waits; wait for the data to be prepared
+        call .reads; read a sector
+        pop cx; restore cx
+        loop .read
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2; = nop, jump to next line, but spend more time cycle
+            jmp $+2; delay
+            jmp $+2
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000
+            jnz .check
+        ret
+
+    .reads:
+        mov dx, 0x1f0
+        mov cx, 256; one sector, 256 bytes
+        .readw:
+            in ax, dx
+            jmp $+2; delay
+            jmp $+2
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret
+
+
+write_disk:
+
+    ; set the number of sectors
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx; 0x1f3
+    mov al, cl; first 8 bits of start sector
+    out dx, al
+
+    inc dx; 0x1f4
+    shr ecx, 8
+    mov al, cl; second 8 bits of start sector
+    out dx, al
+
+    inc dx; 0x1f5
+    shr ecx, 8
+    mov al, cl; third 8 bits of start sector
+    out dx, al
+
+    inc dx; 0x1f6
+    shr ecx, 8
+    and cl, 0b1111; set the high 4 bits to be 0
+
+    mov al, 0b1110_0000
+    ;         7654 4321
+    ; 4: 0, master disk
+    ; 5/7: 1, fixed 1
+    ; 6: 1, LBA mode
+    or al, cl
+    out dx, al; master disk - LBA mode
+
+    inc dx; 0x1f7
+    mov al, 0x30; write disk
+    out dx, al
+
+    xor ecx, ecx; use xor to clear ecx, = mov ecx, 0
+    mov cl, bl; get the number of sector read
+
+    .write:
+        push cx; save cx
+        call .writes; write a sector
+        call .waits; wait for the disk buzy finishing
+        pop cx; restore cx
+        loop .write
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check:
+            in al, dx
+            jmp $+2; = nop, jump to next line, but spend more time cycle
+            jmp $+2; delay
+            jmp $+2
+            and al, 0b1000_0000
+            cmp al, 0b0000_0000
+            jnz .check
+        ret
+
+    .writes:
+        mov dx, 0x1f0
+        mov cx, 256; one sector, 256 bytes
+        .writew:
+            mov ax, [edi]
+            out dx, ax
+            jmp $+2; delay
+            jmp $+2
+            jmp $+2
+            add edi, 2
+            loop .writew
+        ret
+
 
 print:
     mov ah, 0x0e
